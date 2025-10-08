90f95fe7126579588b7b614b3e659a470b9223a1
bharatroy4D <bharatroy1005@gmail.com>
2025-10-08 19:24:40 +0600

    Background grid animation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,73 @@
-import React from 'react';
-import Navbar from './Components/navbar/Navbar';
-import About from './Pages/about/About';
-import Contact from './Pages/contact/Contact';
-import Footer from './Components/footer/Footer';
-import Services from './Pages/services/Services';
-import Projects from './Pages/projects/Projects';
-import Banner from './Pages/banner/Banner';
-import ScrollToTop from './Components/ScrollToTop/ScrollToTop';
+import React from "react";
+import Navbar from "./Components/navbar/Navbar";
+import About from "./Pages/about/About";
+import Contact from "./Pages/contact/Contact";
+import Footer from "./Components/footer/Footer";
+import Services from "./Pages/services/Services";
+import Projects from "./Pages/projects/Projects";
+import Banner from "./Pages/banner/Banner";
+import ScrollToTop from "./Components/ScrollToTop/ScrollToTop";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <div className="min-h-screen relative overflow-hidden">
-        {/* Animated Background Grid Lines */}
-        <div className="absolute -z-10 inset-0 overflow-hidden opacity-20">
-          <svg className="absolute w-full h-full">
+    <div className="relative min-h-screen overflow-hidden">
+      {/* Animated Background Grid Lines */}
+      <div className="absolute -z-10 inset-0 overflow-hidden opacity-20">
+        <div className="grid-wrapper">
+          <svg className="grid-svg">
             <defs>
-              <pattern id="grid" width="30" height="30" patternUnits="userSpaceOnUse">
+              <pattern
+                id="grid"
+                width="30"
+                height="30"
+                patternUnits="userSpaceOnUse"
+              >
                 <path
-                  d="M 50 0 L 0 0 0 50"
+                  d="M 30 0 L 0 0 0 30"
                   fill="none"
-                  stroke="rgba(96, 165, 250, 1)"
+                  stroke="rgba(96, 165, 250, 0.8)"
                   strokeWidth="1"
                 />
               </pattern>
             </defs>
             <rect width="100%" height="100%" fill="url(#grid)" />
           </svg>
+
+          {/* Duplicate for seamless effect */}
+          <svg className="grid-svg">
+            <defs>
+              <pattern
+                id="grid2"
+                width="30"
+                height="30"
+                patternUnits="userSpaceOnUse"
+              >
+                <path
+                  d="M 30 0 L 0 0 0 30"
+                  fill="none"
+                  stroke="rgba(96, 165, 250, 0.8)"
+                  strokeWidth="1"
+                />
+              </pattern>
+            </defs>
+            <rect width="100%" height="100%" fill="url(#grid2)" />
+          </svg>
         </div>
-        <div className='container mx-auto  py-6 px-4 lg:px-15 poppins'>
-          <Navbar />
-          <Banner />
-          <About />
-          <Services />
-          <Projects />
-          <Contact />
-        </div>
-        <Footer />
-        <ScrollToTop />
       </div>
+
+      {/* Page Content */}
+      <div className="container mx-auto py-6 px-4 lg:px-15 poppins">
+        <Navbar />
+        <Banner />
+        <About />
+        <Services />
+        <Projects />
+        <Contact />
+      </div>
+
+      <Footer />
+      <ScrollToTop />
     </div>
   );
 };
